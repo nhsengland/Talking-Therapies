5227397f2085266c8f381343ba89bd69ef5214fe
sarahblincko <sarah.blincko1@nhs.net>
2023-10-05 17:33:33 +0100

    Version 2 Updates

--- UDAL Employment Support Indicator FINAL 230523.sql
+++ UDAL Employment Support Indicator FINAL 230523.sql
@@ -31,14 +31,18 @@ SELECT DISTINCT
 	,CASE WHEN EmpSupportInd='Y' AND r.PathwayID IS NOT NULL THEN 1 ELSE 0 END AS EmpSuppIndYes	--Flag for those who are eligible for employment support
 	,CASE WHEN EmpSupportInd='Y' AND EmploymentSupport_Count>0 AND r.PathwayID IS NOT NULL THEN 1 ELSE 0 END AS EmpSuppFirstAppAndEmpSuppIndYes
 	--Flag for those who are eligible for employment support and have had at least one employment support appointment
-		
-	,ch.Organisation_Code as 'Sub-ICBCode'
-	,ch.Organisation_Name as 'Sub-ICB Name'
-	,ch.STP_Name as 'ICB Name'
-	,ch.Region_Name as 'RegionNameComm'
-	,ph.Organisation_Code as 'ProviderCode'
-	,ph.Organisation_Name as 'Provider Name'
-	,ph.Region_Name as 'RegionNameProv'
+	
+	--Geography
+	,CASE WHEN ch.[Organisation_Code] IS NOT NULL THEN ch.[Organisation_Code] ELSE 'Other' END AS 'Sub-ICBCode'
+	,CASE WHEN ch.[Organisation_Name] IS NOT NULL THEN ch.[Organisation_Name] ELSE 'Other' END AS 'Sub-ICBName'
+	,CASE WHEN ch.[STP_Code] IS NOT NULL THEN ch.[STP_Code] ELSE 'Other' END AS 'ICBCode'
+	,CASE WHEN ch.[STP_Name] IS NOT NULL THEN ch.[STP_Name] ELSE 'Other' END AS 'ICBName'
+	,CASE WHEN ch.[Region_Name] IS NOT NULL THEN ch.[Region_Name] ELSE 'Other' END AS'RegionNameComm'
+	,CASE WHEN ch.[Region_Code] IS NOT NULL THEN ch.[Region_Code] ELSE 'Other' END AS 'RegionCodeComm'
+	,CASE WHEN ph.[Organisation_Code] IS NOT NULL THEN ph.[Organisation_Code] ELSE 'Other' END AS 'ProviderCode'
+	,CASE WHEN ph.[Organisation_Name] IS NOT NULL THEN ph.[Organisation_Name] ELSE 'Other' END AS 'ProviderName'
+	,CASE WHEN ph.[Region_Name] IS NOT NULL THEN ph.[Region_Name] ELSE 'Other' END AS 'RegionNameProv'
+	
 	,r.EmploymentSupport_Count
 INTO [MHDInternal].[TEMP_TTAD_EmpSupp_Indicator_Base]
 FROM [mesh_IAPT].[IDS101referral] r
@@ -46,13 +50,16 @@ FROM [mesh_IAPT].[IDS101referral] r
 	--Allows filtering for the latest data	
 	LEFT JOIN [mesh_IAPT].[IDS004empstatus] emp ON r.recordnumber = emp.recordnumber
 	--Provides data for employment status and other indicators
-	LEFT JOIN [MHDInternal].[REFERENCE_CCG_2020_Lookup] c ON r.OrgIDComm = c.IC_CCG					
-	LEFT JOIN [Reporting].[Ref_ODS_Commissioner_Hierarchies_ICB] ch ON c.CCG21 = ch.Organisation_Code AND ch.Effective_To IS NULL
-	LEFT JOIN [Reporting].[Ref_ODS_Provider_Hierarchies_ICB] ph ON r.OrgID_Provider = ph.Organisation_Code AND ph.Effective_To IS NULL
-	--Three tables for getting the up-to-date Sub-ICB/ICB/Region/Provider names/codes
+	LEFT JOIN [Internal_Reference].[ComCodeChanges] cc ON r.OrgIDComm = cc.Org_Code COLLATE database_default
+	LEFT JOIN [Reporting].[Ref_ODS_Commissioner_Hierarchies_ICB] ch ON COALESCE(cc.New_Code, r.OrgIDComm) = ch.Organisation_Code COLLATE database_default 
+		AND ch.Effective_To IS NULL
+	LEFT JOIN [Internal_Reference].[Provider_Successor] ps ON r.OrgID_Provider = ps.Prov_original COLLATE database_default
+	LEFT JOIN [Reporting].[Ref_ODS_Provider_Hierarchies_ICB] ph ON COALESCE(ps.Prov_Successor, r.OrgID_Provider) = ph.Organisation_Code COLLATE database_default
+		AND ph.Effective_To IS NULL
+	--Four tables for getting the up-to-date Sub-ICB/ICB/Region/Provider names/codes
 WHERE UsePathway_Flag = 'True' 
 	AND IsLatest = 1	--To get the latest data
-	AND l.[ReportingPeriodStartDate] BETWEEN DATEADD(MONTH, 0, @PeriodStart) AND @PeriodStart	--for refresh, the offset should be 0 as only want the data for the latest month
+	AND l.[ReportingPeriodStartDate] BETWEEN DATEADD(MONTH, -33, @PeriodStart) AND @PeriodStart	--for refresh, the offset should be 0 as only want the data for the latest month
 	AND emp.EmpSupportInd='Y'	--Only looking at those who are eligible for employment support
 	AND r.ServDischDate BETWEEN l.ReportingPeriodStartDate AND l.ReportingPeriodEndDate	--Only looking at discharges within the reporting period
 
@@ -61,48 +68,48 @@ WHERE UsePathway_Flag = 'True'
 --This table sums the flags produced in the base table above at Provider, Sub-ICB, ICB and National levels. 
 
 ------------------Provider
---IF OBJECT_ID ('[MHDInternal].[DASHBOARD_TTAD_EmpSupp_Indicator]') IS NOT NULL DROP TABLE [MHDInternal].[DASHBOARD_TTAD_EmpSupp_Indicator]
-INSERT INTO [MHDInternal].[DASHBOARD_TTAD_EmpSupp_Indicator]
+IF OBJECT_ID ('[MHDInternal].[DASHBOARD_TTAD_EmpSupp_Indicator]') IS NOT NULL DROP TABLE [MHDInternal].[DASHBOARD_TTAD_EmpSupp_Indicator]
+--INSERT INTO [MHDInternal].[DASHBOARD_TTAD_EmpSupp_Indicator]
 SELECT
 	Month
 	,cast('Provider' as varchar(max)) as [OrgType]
-	,cast([Provider Name] as varchar(max)) as [OrgName]
+	,cast([ProviderName] as varchar(max)) as [OrgName]
 	,cast([RegionNameProv] as varchar(max)) as [Region]
 	,SUM(FinishedTreatEmpSuppFirstAppAndEmpSuppIndYes) as FinishedTreatEmpSuppFirstAppAndEmpSuppIndYes
 	,SUM(FinishedTreatEmpSuppIndYes) as FinishedTreatEmpSuppIndYes
 	,SUM(EmpSuppFirstAppAndEmpSuppIndYes) as EmpSuppFirstAppAndEmpSuppIndYes
 	,SUM(EmpSuppIndYes) as EmpSuppIndYes
---INTO [MHDInternal].[DASHBOARD_TTAD_EmpSupp_Indicator]
+INTO [MHDInternal].[DASHBOARD_TTAD_EmpSupp_Indicator]
 FROM [MHDInternal].[TEMP_TTAD_EmpSupp_Indicator_Base]
-GROUP BY Month, [Provider Name], [RegionNameProv]
+GROUP BY Month, [ProviderName], [RegionNameProv]
 
 ------------------Sub-ICB
 INSERT INTO [MHDInternal].[DASHBOARD_TTAD_EmpSupp_Indicator]
 SELECT
 	Month
 	,cast('Sub-ICB' as varchar(max)) as [OrgType]
-	,cast([Sub-ICB Name] as varchar(max)) as [OrgName]
+	,cast([Sub-ICBName] as varchar(max)) as [OrgName]
 	,cast([RegionNameComm] as varchar(max)) as [Region]
 	,SUM(FinishedTreatEmpSuppFirstAppAndEmpSuppIndYes) as FinishedTreatEmpSuppFirstAppAndEmpSuppIndYes
 	,SUM(FinishedTreatEmpSuppIndYes) as FinishedTreatEmpSuppIndYes
 	,SUM(EmpSuppFirstAppAndEmpSuppIndYes) as EmpSuppFirstAppAndEmpSuppIndYes
 	,SUM(EmpSuppIndYes) as EmpSuppIndYes
 FROM [MHDInternal].[TEMP_TTAD_EmpSupp_Indicator_Base]
-GROUP BY Month, [Sub-ICB Name], [RegionNameComm]
+GROUP BY Month, [Sub-ICBName], [RegionNameComm]
 
 ------------------ICB
 INSERT INTO [MHDInternal].[DASHBOARD_TTAD_EmpSupp_Indicator]
 SELECT
 	Month
 	,cast('ICB' as varchar(max)) as [OrgType]
-	,cast([ICB Name] as varchar(max)) as [OrgName]
+	,cast([ICBName] as varchar(max)) as [OrgName]
 	,cast([RegionNameComm] as varchar(max)) as [Region]
 	,SUM(FinishedTreatEmpSuppFirstAppAndEmpSuppIndYes) as FinishedTreatEmpSuppFirstAppAndEmpSuppIndYes
 	,SUM(FinishedTreatEmpSuppIndYes) as FinishedTreatEmpSuppIndYes
 	,SUM(EmpSuppFirstAppAndEmpSuppIndYes) as EmpSuppFirstAppAndEmpSuppIndYes
 	,SUM(EmpSuppIndYes) as EmpSuppIndYes
 FROM [MHDInternal].[TEMP_TTAD_EmpSupp_Indicator_Base]
-GROUP BY Month, [ICB Name], [RegionNameComm]
+GROUP BY Month, [ICBName], [RegionNameComm]
 
 ------------------National
 INSERT INTO [MHDInternal].[DASHBOARD_TTAD_EmpSupp_Indicator]
