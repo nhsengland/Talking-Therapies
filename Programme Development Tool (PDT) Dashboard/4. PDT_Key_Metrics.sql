8d27bce0a758ea87c35a3da3cfbd76fc299ed4e7
Alex Macdonald <alxmacdnld@gmail.com>
2023-12-07 13:13:14 +0000

    Update 4. PDT_Key_Metrics.sql
    Updated script for single submission window

--- Programme Development Tool (PDT) Dashboard/4. PDT_Key_Metrics.sql
+++ Programme Development Tool (PDT) Dashboard/4. PDT_Key_Metrics.sql
@@ -1,9 +1,9 @@
--- SET ANSI_WARNINGS OFF
--- SET NOCOUNT ON
+SET ANSI_WARNINGS OFF
+SET NOCOUNT ON
 
 -- Refresh updates for [MHDInternal].[DASHBOARD_TTAD_PDT_KeyMetrics] -----------------------------
 
-DECLARE @Offset AS INT = -1
+DECLARE @Offset AS INT = 0
 
 DECLARE @PeriodStart DATE = (SELECT DATEADD(MONTH,@Offset,MAX([ReportingPeriodStartDate])) FROM [mesh_IAPT].[IsLatest_SubmissionID])
 DECLARE @PeriodEnd DATE = (SELECT EOMONTH(DATEADD(MONTH,@Offset,MAX([ReportingPeriodEndDate]))) FROM [mesh_IAPT].[IsLatest_SubmissionID])
@@ -12,11 +12,10 @@ DECLARE @MonthYear VARCHAR(50) = (DATENAME(M, @PeriodStart) + ' ' + CAST(DATEPAR
 PRINT CHAR(10) + 'Month: ' + CAST(@MonthYear AS VARCHAR(50)) + CHAR(10)
 
 ---------------------------------------------------------------------------------------------------------------------------------------------
---IF OBJECT_ID('[MHDInternal].[DASHBOARD_TTAD_PDT_KeyMetrics]') IS NOT NULL DROP TABLE [MHDInternal].[DASHBOARD_TTAD_PDT_KeyMetrics]
+
 INSERT INTO [MHDInternal].[DASHBOARD_TTAD_PDT_KeyMetrics]
-SELECT *
---INTO [MHDInternal].[DASHBOARD_TTAD_PDT_KeyMetrics]
-FROM(
+
+SELECT * FROM (
 
 	SELECT	[Month]
 			,[DataSource]
@@ -39,7 +38,8 @@ FROM(
 
 	UNPIVOT ([Value] FOR [Measure] IN ([Finished Treatment - 2 or more Apps], [Referrals], [EnteringTreatment])) u
 
-	WHERE	[Level] <> 'CCG/ Provider' AND [Month] = @MonthYear
+	WHERE	[Level] <> 'CCG/ Provider' 
+		AND ([Month] = @MonthYear OR [Month] = DATEADD(MONTH, -1, @MonthYear))
 
 	UNION -----------------------------------------------------------------------
 
@@ -64,7 +64,8 @@ FROM(
 
 	UNPIVOT ([Value] FOR [Measure] IN ([FinishedCourseTreatment6WeeksRate], [FinishedCourseTreatment18WeeksRate], [RecoveryRate], [ReliableImprovementRate], [OpenReferral90daysRate], [FirsttoSecond90daysRate])) u
 
-	WHERE	[Level] <> 'CCG/ Provider' AND [Month] = @MonthYear
+	WHERE	[Level] <> 'CCG/ Provider' 
+		AND ([Month] = @MonthYear OR [Month] = DATEADD(MONTH, -1, @MonthYear))
 
 	UNION -----------------------------------------------------------------------
 
@@ -89,7 +90,7 @@ FROM(
 
 	UNPIVOT ([Value] FOR [Measure] IN ([RecRate])) u
 
-	WHERE [Month] = @MonthYear
+	WHERE [Month] = @MonthYear OR [Month] = DATEADD(MONTH, -1, @MonthYear)
 
 	UNION -----------------------------------------------------------------------
 
@@ -114,7 +115,8 @@ FROM(
 
 	UNPIVOT ([Value] FOR [Measure] IN ([EnteringTreatment])) u
 
-	WHERE [Month] = @MonthYear
+	WHERE [Month] = @MonthYear OR [Month] = DATEADD(MONTH, -1, @MonthYear)
+
 )_
 
 -----------------------------------------------------------------------------------------------------------
